0b3096a721cfeeeaf3825de715859a5db484d198
Franky <CTFFranky@gmail.com>
2014-07-26 16:23:07 +0800

    Updated script in support of leveling
    Adding namespace function toward all level, and added the ability to do
    nested declaration

--- namespace.js
+++ namespace.js
@@ -3,30 +3,33 @@
     *
     * @param {String} namespace to be created
     * @return {Object} an object pointing to the namespace
-    * 
+    *
     * Example :
     *   var ns = antz.namespace('antz.module.Booking');
-    *   ns === antz.module.Booking;  // true
+    *   var ui = ns.namespace('ui');
+    *
+    *   ns === antz.module.Booking;     // true
+    *   ui === antz.module.Booking.ui;  // true
     *
     *   ns.NewClass = function(){ };
     */
-      (function(path){
+    (function(path){
         'use strict';
 
-        var root_obj = eval("window."+path+" = window."+path+" || {}");
+        var root_obj = window[path] = window[path] || {};
 
         function Child(){};
 
         if(root_obj)
-            Child = root_obj.constructor;
+            root_obj.namespace = runNamespace;
         else
-            root_obj = new Child();
+            root_obj = window[path] = new Child();
 
-        function importNamespace(ns) { 
+        function importNamespace(ns, ns_child) {
             var parts = ns.split('.')
-              , namespace = root_obj;
+              , namespace = ns_child || root_obj;
 
-            var i = (parts[0] === path)? 1 : 0; 
+            var i = (parts[0] === path)? 1 : 0;
 
             for (var len = parts.length; i < len; i++) {
                 var _child = parts[i];
@@ -34,13 +37,15 @@
                 namespace = ( namespace[_child] = namespace[_child] || new Child() );
             }
 
-            return namespace; 
+            return namespace;
         }
 
-        Child.prototype.namespace = function(ns_string, get_ns){
-            var exportNamspace = importNamespace(ns_string);
+        function runNamespace(ns_string, get_ns){
+            var exportNamspace = importNamespace(ns_string, this);
             if(get_ns) get_ns(exportNamspace, importNamespace);
             return exportNamspace;
         }
+
+        Child.prototype.namespace = runNamespace
 
     }("antz"));
